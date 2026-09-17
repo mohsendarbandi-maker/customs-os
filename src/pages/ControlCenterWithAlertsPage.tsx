ed1761143827affb65157a344ba3accfbb0da60f
mohsendarbandi-maker <mohsen.darbandi@gmail.com>
2026-09-17 16:34:13 +0330

    feat(stage-25): compose control center with live case alerts

---
+++ src/pages/ControlCenterWithAlertsPage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import {CaseAlertPanel} from './CaseAlertPanel';
+import {ControlCenterPage} from './ControlCenterPage';
+
+export const ControlCenterWithAlertsPage:React.FC=()=> <><CaseAlertPanel/><ControlCenterPage/></>;
